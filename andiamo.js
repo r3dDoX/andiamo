aa5e08b995de60460dd22af18502139a4e797fa7
r3dDoX <Patrick@Elfriede-Pro.local>
2014-04-21 23:15:19 +0200

    Added functionalty to request pages from server

--- andiamo.js
+++ andiamo.js
@@ -2,24 +2,14 @@ if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to andiamo.";
   };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-      Meteor.http.get("http://www.google.com", function(err, result) {
-          if (!err) {
-            console.log('awesome');
-          } else {
-              console.log('shit');
-          }
-      });
-  });
+    Meteor.methods({
+        importData: function() {
+            var url = 'http://www.sfl.ch/superleague/matchcenter/';
+
+            return Meteor.http.get(url);
+        }
+    });
 }
