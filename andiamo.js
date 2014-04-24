80a098a877eb119bac8416f22c68769a576d9e55
r3dDoX <Patrick@Elfriede-Pro.local>
2014-04-24 23:22:14 +0200

    Added parsing of fifa page

--- andiamo.js
+++ andiamo.js
@@ -10,9 +10,31 @@ if (Meteor.isServer) {
     Meteor.methods({
         importData: function() {
             var url = 'http://www.fifa.com/worldcup/matches/index.html',
-                $ = cheerio.load(Meteor.http.get(url).content);
+                $ = cheerio.load(Meteor.http.get(url).content),
+                matches = new Array(),
+                actMatch,
+                timeParts;
 
-            return $('.match-list .fixture .s-scoreText').text();
+            $('.match-list .fixture').each(function(index, element) {
+                actMatch = {};
+                actMatch.id = $(element).attr('id');
+                actMatch.date = new Date($('.mu-i-date', element).text() + " UTC");
+
+                timeParts = $('.s-date-HHmm', element).data('timeutc').split(':');
+                actMatch.date.setUTCHours(timeParts[0], timeParts[1]);
+
+                actMatch.location = {};
+                actMatch.location.stadium = $('.mu-i-stadium', element).text();
+                actMatch.location.venue = $('.mu-i-venue', element).text();
+
+                actMatch.homeTeam = $('.home .t-nText', element).text();
+                actMatch.awayTeam = $('.away .t-nText', element).text();
+                actMatch.group = $('.mu-i-group', element).text();
+
+                matches.push(actMatch);
+            });
+
+            return matches;
         }
     });
 }
