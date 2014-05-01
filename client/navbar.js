ec77111a8beb3be5d69be0fb47274875319bec66
r3dDoX <Patrick@Elfriede-Pro.local>
2014-05-01 14:16:07 +0200

    changed navbar to allow dropdown menu and therefor changed include of bootstrap.min.js

--- client/navbar.js
+++ client/navbar.js
@@ -1,15 +1,22 @@
 Template.navbar.menuElements = function () {
     return [
-        { text: 'My Tips', pageId: 'myTips' }
+        {
+            text: 'My Tips',
+            pageId: 'myTips',
+            children: [
+                { text: 'Worldcup', pageId: 'worldCup' },
+                { text: 'Super League', pageId: 'superLeague' }
+            ]
+        }
     ];
 };
 
-Template.navbar.isSelectedMenuElement = function(actualId) {
+Template.navbarElement.isSelectedMenuElement = function(actualId) {
     return Session.get('selectedMenuElement') === actualId;
 };
 
 Template.navbar.events({
-    'click a': function (event) {
+    'click a.navbar-brand, click a.elementLink': function (event) {
         event.preventDefault();
         Router.navigate("pages/" + event.target.getAttribute('data-pageId'), {trigger: true});
     }
