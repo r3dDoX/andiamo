986e47972641dd2b96a00d101db9c63e3fe742f5
r3dDoX <patrick@walther-online.ch>
2014-05-09 15:54:21 +0200

    refactored button to template

--- client/pages/import/importSuperLeague.js
+++ client/pages/import/importSuperLeague.js
@@ -18,23 +18,3 @@ Template.importSuperLeague.matches = function () {
 Template.importSuperLeague.teams = function () {
     return TablesSuperLeague.find().fetch();
 };
-
-Template.importSuperLeague.events({
-    'click button#importSuperLeague': function (event) {
-        var button = event.target;
-        button.classList.add('btn-primary');
-        button.classList.add('disabled');
-        
-        Meteor.call('importSuperLeague', function (error, success) {
-            button.classList.remove('disabled');
-            button.classList.remove('btn-primary');
-            
-            if (error) {
-                button.classList.add('btn-error');
-                console.log(error);
-            } else {
-                button.classList.add('btn-success');
-            }
-        });
-    }
-});
