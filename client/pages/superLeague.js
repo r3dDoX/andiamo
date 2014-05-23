43192635dfa8bd8df09fd4fc2f6947605b475132
r3dDoX <patrick@walther-online.ch>
2014-05-24 01:48:28 +0200

    removed owl carousel because it isn't designed to hold 180 items

--- client/pages/superLeague.js
+++ client/pages/superLeague.js
@@ -2,16 +2,6 @@
 /*global $, MatchesSuperLeague, Template */
 'use strict';
 
-Template.superLeague.rendered = function () {
-    $("#superLeagueMatches").owlCarousel({
-        items: 5,
-        itemsDesktop : [1000, 4],
-        itemsDesktopSmall : [800, 3],
-        itemsTablet: [600, 2],
-        itemsMobile: [479, 1]
-    });
-};
-
 Template.superLeague.matches = function () {
-    return MatchesSuperLeague.find().fetch().slice(0, 5);
+    return MatchesSuperLeague.find().fetch();
 };
