b8693e7c9c6593d769248881701d68909b5a8fac
r3dDoX <patrick@walther-online.ch>
2014-06-13 20:48:22 +0200

    fixed wrong css class name

--- client/pages/worldcup.js
+++ client/pages/worldcup.js
@@ -257,7 +257,7 @@ Template.allTipsWorldcup.getCssClass = function (points) {
     
     switch (points) {
     case 0:
-        return cssClass + 'lable-error';
+        return cssClass + 'lable-danger';
     case 2:
     case 3:
         return cssClass + 'label-warning';
