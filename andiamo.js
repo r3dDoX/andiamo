277383e35d78743c8bd06570cc9a6a2ea0580a6d
r3dDoX <patrick@walther-online.ch>
2014-06-13 23:51:40 +0200

    another shot at IE10 mobile bug

---
+++ andiamo.js
@@ -0,0 +1,12 @@
+/*jslint node: true */
+/*global Meteor */
+'use strict';
+
+if (Meteor.isClient) {
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement("style");
+        msViewportStyle.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"));
+        
+        document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+    }
+}
