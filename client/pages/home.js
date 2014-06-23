25648e325fbb84f69cdb6d5abf5ec864dd753209
r3dDoX <patrick@walther-online.ch>
2014-06-23 21:11:56 +0200

    moved ranking table calculation to server

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,5 +1,5 @@
 /*jslint node: true, nomen: true, plusplus: true */
-/*global Meteor, Template, MatchesWorldcup:true, TipsWorldcup:true */
+/*global Meteor, Template, StandingsWorldcup:true */
 'use strict';
 
 var scrollElementId = 'home',
@@ -9,92 +9,6 @@ var scrollElementId = 'home',
         if (bgPic && pageElement.scrollTop < bgPic.clientHeight) {
             bgPic.style.backgroundPositionY = (initialPositionY - pageElement.scrollTop / 25) + '%';
         }
-    },
-    
-    getPointsFromFinalsTips = function (user) {
-        var points = 0,
-            first,
-            second,
-            third,
-            tip,
-            bigFinal = MatchesWorldcup.findOne({group: 'Final', isFinished: true}),
-            smallFinal = MatchesWorldcup.findOne({group: 'Play-off for third place', isFinished: true});
-        
-        if (bigFinal) {
-            if (bigFinal.homeScore > bigFinal.awayScore) {
-                first = bigFinal.homeTeam;
-                second = bigFinal.awayTeam;
-            } else {
-                first = bigFinal.awayTeam;
-                second = bigFinal.homeTeam;
-            }
-            
-            tip =  TipsWorldcup.findOne({user: user._id, rank: 'first'});
-            if (tip && tip.team === first) {
-                points += 30;
-            }
-            
-            tip = TipsWorldcup.findOne({user: user._id, rank: 'second'});
-            if (tip && tip.team === second) {
-                points += 20;
-            }
-        }
-        
-        if (smallFinal) {
-            if (smallFinal.homeScore > smallFinal.awayScore) {
-                third = smallFinal.homeTeam;
-            } else {
-                third = smallFinal.awayTeam;
-            }
-
-            tip = TipsWorldcup.findOne({user: user._id, rank: 'third'});
-            if (tip && tip.team === third) {
-                points += 10;
-            }
-        }
-        
-        return points;
-    },
-
-    sortStandingsTable = function (standingsTable) {
-        standingsTable.sort(function (a, b) {
-            return b.points - a.points;
-        });
-    },
-    
-    countPoints = function (sum, item) {
-        return (sum.points || sum) + item.points;
-    },
-
-    standingsTable = function () {
-        var usersWithRanking = Meteor.users.find({}).fetch(),
-            rank = 0,
-            lastPoints;
-
-        usersWithRanking.map(function (user) {
-            var tips = TipsWorldcup.find({user: user._id, points: {$exists: true}}).fetch() || [],
-                points = tips.reduce(countPoints, 0);
-            
-            points += getPointsFromFinalsTips(user);
-
-            user.points = points;
-            return user;
-        });
-
-        sortStandingsTable(usersWithRanking);
-
-        usersWithRanking.map(function (element) {
-            if (element.points === lastPoints) {
-                element.rank = rank;
-            } else {
-                element.rank = ++rank;
-                lastPoints = element.points;
-            }
-            
-            return element;
-        });
-
-        return usersWithRanking;
     };
 
 Template.home.rendered = function () {
@@ -112,5 +26,16 @@ Template.home.rendered = function () {
 };
 
 Template.standingsTable.userRankings = function () {
-    return standingsTable();
+    var rank = 0,
+        lastPoints;
+    
+    return StandingsWorldcup.find({}, { sort: {points: -1}}).fetch().map(function (userStanding) {
+        if (userStanding.points === lastPoints) {
+            userStanding.rank = rank;
+        } else {
+            userStanding.rank = ++rank;
+        }
+        
+        return userStanding;
+    });
 };
--- leagues/worldcup.js
+++ leagues/worldcup.js
@@ -1,9 +1,10 @@
 /*jslint node: true, nomen: true */
-/*global Meteor, MatchesWorldcup:true, TablesWorldcup:true, TipsWorldcup:true, check */
+/*global Meteor, MatchesWorldcup:true, TablesWorldcup:true, TipsWorldcup:true, StandingsWorldcup:true, check */
 
 MatchesWorldcup = new Meteor.Collection('matchesWorldcup');
 TablesWorldcup = new Meteor.Collection('tablesWorldcup');
 TipsWorldcup = new Meteor.Collection('tipsWorldcup');
+StandingsWorldcup = new Meteor.Collection('standingsWorldcup');
 
 if (Meteor.isServer) {
     'use strict';
@@ -132,11 +133,6 @@ if (Meteor.isServer) {
                 TablesWorldcup.upsert({ id: actGroup.id }, { $set: actGroup });
             });
         },
-
-        importWorldcup = function () {
-            parseTables();
-            parseMatches();
-        },
         
         saveTip = function (tip) {
             check(tip, Object);
@@ -160,6 +156,78 @@ if (Meteor.isServer) {
             }
             
             TipsWorldcup.upsert({ user: tip.user, rank: tip.rank}, { $set: tip });
+        },
+    
+        getPointsFromFinalsTips = function (user) {
+            var points = 0,
+                first,
+                second,
+                third,
+                tip,
+                bigFinal = MatchesWorldcup.findOne({group: 'Final', isFinished: true}),
+                smallFinal = MatchesWorldcup.findOne({group: 'Play-off for third place', isFinished: true});
+
+            if (bigFinal) {
+                if (bigFinal.homeScore > bigFinal.awayScore) {
+                    first = bigFinal.homeTeam;
+                    second = bigFinal.awayTeam;
+                } else {
+                    first = bigFinal.awayTeam;
+                    second = bigFinal.homeTeam;
+                }
+
+                tip =  TipsWorldcup.findOne({user: user._id, rank: 'first'});
+                if (tip && tip.team === first) {
+                    points += 30;
+                }
+
+                tip = TipsWorldcup.findOne({user: user._id, rank: 'second'});
+                if (tip && tip.team === second) {
+                    points += 20;
+                }
+            }
+
+            if (smallFinal) {
+                if (smallFinal.homeScore > smallFinal.awayScore) {
+                    third = smallFinal.homeTeam;
+                } else {
+                    third = smallFinal.awayTeam;
+                }
+
+                tip = TipsWorldcup.findOne({user: user._id, rank: 'third'});
+                if (tip && tip.team === third) {
+                    points += 10;
+                }
+            }
+
+            return points;
+        },
+
+        countPoints = function (sum, item) {
+            return (sum.points || sum) + item.points;
+        },
+
+        updateStandingsTable = function () {
+            var usersWithRanking = Meteor.users.find({}).fetch(),
+                rank = 0,
+                lastPoints;
+
+            usersWithRanking.forEach(function (user) {
+                var tips = TipsWorldcup.find({user: user._id, points: {$exists: true}}).fetch() || [],
+                    points = tips.reduce(countPoints, 0);
+
+                points += getPointsFromFinalsTips(user);
+
+                user.points = points;
+                
+                StandingsWorldcup.upsert({ username: user.username }, { $set: { username: user.username, points: points} });
+            });
+        },
+    
+        importWorldcup = function () {
+            parseTables();
+            parseMatches();
+            updateStandingsTable();
         };
 
     //publish collections
@@ -174,6 +242,10 @@ if (Meteor.isServer) {
     Meteor.publish('tipsWorldcup', function () {
         return TipsWorldcup.find();
     });
+    
+    Meteor.publish('standingsWorldcup', function () {
+        return StandingsWorldcup.find();
+    });
 
     //export Methods
     Meteor.methods({
@@ -187,4 +259,5 @@ if (Meteor.isClient) {
     Meteor.subscribe('matchesWorldcup');
     Meteor.subscribe('tablesWorldcup');
     Meteor.subscribe('tipsWorldcup');
+    Meteor.subscribe('standingsWorldcup');
 }
