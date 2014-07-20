094a402b0f7a52bc8d6b2dcae6cf1fd9c6c8583a
r3dDoX <patrick@walther-online.ch>
2014-07-20 22:37:32 +0200

    major restyling

--- client/pages/home.js
+++ client/pages/home.js
@@ -2,29 +2,6 @@
 /*global Meteor, Template, StandingsSuperLeague:true */
 'use strict';
 
-var scrollElementId = 'home',
-    initialPositionY = 50,
-    
-    scrollBgPic = function (pageElement, bgPic, event) {
-        if (bgPic && pageElement.scrollTop < bgPic.clientHeight) {
-            bgPic.style.backgroundPositionY = (initialPositionY - pageElement.scrollTop / 25) + '%';
-        }
-    };
-
-Template.home.rendered = function () {
-    var pageElement = document.getElementById(scrollElementId),
-        bgPic = pageElement.getElementsByClassName('header')[0],
-        scrollBgPicPartial = scrollBgPic.bind(this, pageElement, bgPic),
-        width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    
-    if (width >= 768) { // if xs device, don't load big Pic
-        bgPic.style.backgroundImage = 'url("/arena.jpg")';
-    }
-    
-    document.getElementById(scrollElementId).addEventListener('scroll', scrollBgPicPartial);
-    document.getElementById(scrollElementId).addEventListener('touchmove', scrollBgPicPartial);
-};
-
 Template.standingsTable.userRankings = function () {
     var rank = 0,
         lastPoints;
