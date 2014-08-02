4e65a2e3b676783ca73fd1317b4e0f46fa132e25
r3dDoX <patrick@walther-online.ch>
2014-08-02 20:40:04 +0200

    added functionality to delete a user

--- app/client/pages/admin.js
+++ app/client/pages/admin.js
@@ -1,4 +1,31 @@
 /*jslint node: true, browser: true */
-/*global Meteor */
+/*global Meteor, Template, Session, $ */
 'use strict';
 
+var hasBeenShown = false;
+
+Template.admin.isShown = function () {
+    if (!hasBeenShown && Session.get('selectedMenuElement') === 'admin') {
+        hasBeenShown = true;
+    }
+    
+    return hasBeenShown;
+};
+
+Template.admin.users = function() {
+    return Meteor.users.find().fetch();
+};
+
+Template.admin.events({
+    'click #userTable button.btn-danger, click #userTable button.btn-warning': function(event) {
+        var button = $(event.target);
+        
+        if (button.hasClass('btn-warning')) {
+            Meteor.call('removeUser', button.data('userid'));
+        } else {
+            button.removeClass('btn-danger');
+            button.addClass('btn-warning');
+            button.html('Are you sure?');
+        }
+    }
+});
--- app/helpers/users.js
+++ app/helpers/users.js
@@ -1,10 +1,23 @@
 /*jslint node: true, nomen: true */
-/*global Meteor */
+/*global check, Meteor, Roles, StandingsSuperLeague */
 'use strict';
 
 if (Meteor.isServer) {
     Meteor.publish("userData", function () {
         return Meteor.users.find({}, {fields: {'_id': 1, 'username': 1}});
+    });
+    
+    Meteor.methods({
+        removeUser: function (userId) {
+            var user;
+            if(Roles.userIsInRole(this.userId, ['admin'])) {
+                check(userId, String);
+                user = Meteor.users.findOne({_id: userId});
+                
+                Meteor.users.remove(userId);
+                StandingsSuperLeague.remove({ username: user.username });
+            }
+        }
     });
 }
 
