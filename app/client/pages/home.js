ba35df51638f242f1860bc806b1c504861176141
r3dDoX <patrick@walther-online.ch>
2014-08-04 22:37:18 +0200

    added proper ranking

--- app/client/pages/home.js
+++ app/client/pages/home.js
@@ -2,14 +2,17 @@
 
 Template.standingsTable.userRankings = function () {
     var rank = 0,
+        usersInRank = 1,
         lastPoints;
     
     return StandingsSuperLeague.find({}, { sort: {points: -1}}).fetch().map(function (userStanding) {
         if (userStanding.points === lastPoints) {
+            usersInRank++;
             userStanding.rank = rank;
         } else {
-            userStanding.rank = ++rank;
+            userStanding.rank = rank += usersInRank;
             lastPoints = userStanding.points;
+            usersInRank = 1;
         }
         
         return userStanding;
