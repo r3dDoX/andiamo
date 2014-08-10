c7c753f2b6eb7f22d9e1e51648139feef3b7cc7d
r3dDoX <patrick@walther-online.ch>
2014-08-10 18:20:36 +0200

    change to setup all old users to get a profile name

--- app/server/httpheaders.js
+++ app/server/httpheaders.js
@@ -7,4 +7,11 @@ Meteor.startup(function () {
         res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
         return next();
     });
+    
+    Meteor.users.find().fetch().forEach(function (user){
+        if(!user.profile) {
+            user.profile = { name: user.username };
+            Meteor.users.update({_id: user._id}, user);
+        }
+    });
 });
