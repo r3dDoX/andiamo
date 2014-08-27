d10c2e9a596bb6b01376e4e1e64d206b08b4195d
r3dDoX <patrick@walther-online.ch>
2014-08-27 21:43:54 +0200

    minimal code format

--- app/shared/leagues/collections/superLeagueCollections.js
+++ app/shared/leagues/collections/superLeagueCollections.js
@@ -4,7 +4,8 @@ TipsSuperLeague = new Meteor.Collection('tipsSuperLeague');
 StandingsSuperLeague = new Meteor.Collection('standingsSuperLeague');
 FlagsSuperLeague = new Meteor.Collection('flagsSuperLeague');
 
-if (Meteor.isServer) { // speed up mongodb with indices
+if (Meteor.isServer) {
+    // speed up mongodb with indices
     MatchesSuperLeague._ensureIndex({'id': 1, 'date': 1, 'isFinished': 1});
     TablesSuperLeague._ensureIndex({'shortName': 1});
     TipsSuperLeague._ensureIndex({'match': 1, 'user': 1});
