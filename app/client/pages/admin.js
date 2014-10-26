9f6e49221c269256a56059789a6b81ffb7e9b835
r3dDoX <patrick@walther-online.ch>
2014-10-26 17:30:39 +0100

    removed deprecated methods from admin

--- app/client/pages/admin.js
+++ app/client/pages/admin.js
@@ -1,13 +1,15 @@
 'use strict';
 
-Template.admin.users = function() {
-    return Meteor.users.find().fetch();
-};
+Template.admin.helpers({
+    users: function () {
+        return Meteor.users.find().fetch();
+    }
+});
 
 Template.admin.events({
-    'click #userTable button.btn-danger, click #userTable button.btn-warning': function(event) {
+    'click #userTable button.btn-danger, click #userTable button.btn-warning': function (event) {
         var button = $(event.target);
-        
+
         if (button.hasClass('btn-warning')) {
             Meteor.call('removeUser', button.data('userid'));
         } else {
