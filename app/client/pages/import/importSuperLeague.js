6eee105b6fa62bde7dfb18ee57fe90b1409b4891
r3dDoX <patrick@walther-online.ch>
2014-10-26 17:30:03 +0100

    removed deprecated methods in importSuperLeague

--- app/client/pages/import/importSuperLeague.js
+++ app/client/pages/import/importSuperLeague.js
@@ -3,20 +3,31 @@
 Meteor.subscribe('matchesSuperLeague');
 Meteor.subscribe('tablesSuperLeague');
 
-Template.importSuperLeague.matches = function () {
-    var matches = MatchesSuperLeague.find({}, {sort: {matchday: 1, date: 1}}).fetch();
-    
-    matches.map(function (element) {
-        if (element.date) {
-            element.date = element.date.toDateString();
-        }
-        
-        return element;
-    });
-    
-    return matches;
-};
+Template.importSuperLeague.helpers({
+    matches: function () {
+        var matches = MatchesSuperLeague.find({}, {
+            sort: {
+                matchday: 1,
+                date: 1
+            }
+        }).fetch();
 
-Template.importSuperLeague.teams = function () {
-    return TablesSuperLeague.find({}, {sort: {rank: 1}}).fetch();
-};
+        matches.map(function (element) {
+            if (element.date) {
+                element.date = element.date.toDateString();
+            }
+
+            return element;
+        });
+
+        return matches;
+    },
+
+    teams: function () {
+        return TablesSuperLeague.find({}, {
+            sort: {
+                rank: 1
+            }
+        }).fetch();
+    }
+});
